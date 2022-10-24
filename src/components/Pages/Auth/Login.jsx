58558a3b869a508c6e0f6b1ec713e0286780a26c
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2022-10-24 14:38:05 +0600

    login signup page pic upload

--- src/components/Pages/Auth/Login.jsx
+++ src/components/Pages/Auth/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
                     <div
                         className='hidden bg-cover lg:block lg:w-2/5'
                         style={{
-                            backgroundImage: `url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')`,
+                            backgroundImage: `url('https://i.ibb.co/tBHLrZv/306164544-707550264382380-1766343590130467835-n.png')`,
                         }}></div>
 
                     <div className='flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5'>
@@ -23,48 +23,6 @@ const Login = () => {
                                 with others also personal account and begin
                                 setting up your profile.
                             </p>
-
-                            <div className='mt-6'>
-                                <h1 className=''>Select type of account</h1>
-
-                                <div className='mt-3 md:flex md:items-center md:-mx-2'>
-                                    <button className='flex justify-center w-full px-6 py-3 mt-4  rounded-md md:mt-0 md:w-auto md:mx-2 btn btn-outline btn-ghost'>
-                                        <svg
-                                            xmlns='http://www.w3.org/2000/svg'
-                                            class='w-6 h-6'
-                                            fill='none'
-                                            viewBox='0 0 24 24'
-                                            stroke='currentColor'
-                                            stroke-width='2'>
-                                            <path
-                                                stroke-linecap='round'
-                                                stroke-linejoin='round'
-                                                d='M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
-                                            />
-                                        </svg>
-
-                                        <span className='mx-2'>User</span>
-                                    </button>
-
-                                    <button className='flex justify-center w-full px-6 py-3 mt-4  rounded-md md:mt-0 md:w-auto md:mx-2 btn btn-outline btn-ghost'>
-                                        <svg
-                                            xmlns='http://www.w3.org/2000/svg'
-                                            className='w-6 h-6'
-                                            fill='none'
-                                            viewBox='0 0 24 24'
-                                            stroke='currentColor'
-                                            stroke-width='2'>
-                                            <path
-                                                stroke-linecap='round'
-                                                stroke-linejoin='round'
-                                                d='M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'
-                                            />
-                                        </svg>
-
-                                        <span className='mx-2'>Doctor</span>
-                                    </button>
-                                </div>
-                            </div>
 
                             <form className='grid grid-cols-1 gap-6 mt-8 md:grid-cols-2'>
                                 <div>
